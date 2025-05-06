68ee6fab13d1730975621abdf04215d14a4b0e16
Erik <erik.liederbach@bszn-da.de>
2025-05-06 08:45:50 +0200

    refactored datasend xbox

--- datasend_xbox.py
+++ datasend_xbox.py
@@ -10,10 +10,15 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 xbox = XboxController()
 
 while True:
-    x, y, x2, y2, a, b, btx, bty, rb, lb, tr, tl = xbox.read()
-    speed = tr - tl
-    data = str(speed)+";"+str(x)+";"+str(x2)
-    print(data)
+    #x, y, x2, y2, a, b, btx, bty, rb, lb, tr, tl = xbox.read()
+    cmd = ""
+    if (xbox.A):
+        cmd = "self_drive"
+    
+    speed = xbox.RightTrigger - xbox.LeftTrigger
+    data = str(speed)+";"+str(xbox.LeftJoystickX)+";"+str(xbox.RightJoystickX)+";"+cmd
+
+    #print(data)
     data = bytes(data,"utf-8")
     sock.sendto(data, (UDP_IP, UDP_PORT))
     time.sleep(0.1)
